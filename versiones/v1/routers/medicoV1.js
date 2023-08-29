e8b0b7c6f80cd177b22728f5cd755b1bc686b977
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-08-28 20:30:45 -0500

    feat(EndPoint): :sparkles: 3 EndPoint
    se realizo el tercer Endpoint que consistia en obtener todos los medicos de una especialidad especifica
    
    by:Carlos Villafrades

--- versiones/v1/routers/medicoV1.js
+++ versiones/v1/routers/medicoV1.js
@@ -1,10 +1,38 @@
 import {Router} from "express";
-import { verifyToken } from "../../../config/jwt.js";
+import { validatePermisos } from "../../../helpers/validatePermisos.js";
+import conexion from "../../../db/connect.js";
+
+const dataBase = await conexion();
 
 let medicoV1 = Router();
 
-medicoV1.get("/", verifyToken("user"), (req,res)=>{
-    res.send("soy medicosss")
+// 3.Obtener todos los médicos de una especialidad específica (por ejemplo, **'Cardiología'**) ?especialidad=""
+
+medicoV1.get("/", validatePermisos("get_medicos"), async (req,res)=>{
+  try {
+
+    let especialidad = req.query.especialidad
+    console.log(especialidad);
+
+    let tabla = dataBase.collection("medico")
+    let data = await tabla.aggregate([
+        {
+            $match: {
+                "med_especialidad": especialidad
+            }
+        },
+        {
+            $project: {
+                _id: 0
+            }
+        }
+    ]).toArray();
+
+    res.send(data)
+
+} catch (error) {
+    res.send({status: 400, message: "Error al consultar la collection"})
+}
 })
 
 export {
