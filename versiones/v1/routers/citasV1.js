ddc05b28060cccf8e9343d2025f4b0909d79c802
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-08-28 20:22:25 -0500

    feat(EndPoint): :sparkles: 2 EndPoint
    se creo el segundo endpoint que consistia en obetner las citas
    
    by: Carlos Villafrades

--- versiones/v1/routers/citasV1.js
+++ versiones/v1/routers/citasV1.js
@@ -1,10 +1,30 @@
 import {Router} from "express";
 import { validatePermisos } from "../../../helpers/validatePermisos.js";
+import conexion from "../../../db/connect.js";
+
+const dataBase = await conexion();
 
 let citasV1 = Router();
 
-citasV1.get("/", validatePermisos("get_citas"), (req,res)=>{
-    res.send("soy citassss")
+// 2.Obtener todas las citas alfabéticamente
+citasV1.get("/", validatePermisos("get_citas"), async (req,res)=>{
+    try {
+
+        let tabla = dataBase.collection("cita")
+        let data = await tabla.aggregate([
+            {
+                $project:{
+                    _id: 0
+                }
+            }
+        ]).toArray();
+
+        res.send(data)
+
+    } catch (error) {
+        res.send({status: 400, message: "Error al obtener lo datos"})
+    }
+    
 })
 
 export {
