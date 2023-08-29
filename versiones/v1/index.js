1b1d81f4a0f77def485ef34585b8e58c0d4d5e42
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-08-28 21:33:33 -0500

    feat: :sparkles: Agregacion del limit
    se agrego el archivo limit para limitar la cantidad de peticiones tanto en el login como en los endpoints
    
    by: Carlos Villafrades

--- versiones/v1/index.js
+++ versiones/v1/index.js
@@ -4,13 +4,14 @@ import { loginV1 } from "./routers/loginV1.js";
 import { citasV1 } from "./routers/citasV1.js";
 import { usuarioV1 } from "./routers/usuariosV1.js";
 import { medicoV1 } from "./routers/medicoV1.js";
+import { limitLogin, limitUsuario } from "../../config/limit.js";
 
 const RoutesV1 = Router();
 
-RoutesV1.use("/login", loginV1)
-RoutesV1.use("/citas",verifyToken(), citasV1)
-RoutesV1.use("/usuarios",verifyToken(), usuarioV1)
-RoutesV1.use("/medicos",verifyToken(), medicoV1)
+RoutesV1.use("/login", limitLogin(), loginV1)
+RoutesV1.use("/citas", limitUsuario(), verifyToken(), citasV1)
+RoutesV1.use("/usuarios", limitUsuario(), verifyToken(), usuarioV1)
+RoutesV1.use("/medicos", limitUsuario(), verifyToken(), medicoV1)
 
 
 
