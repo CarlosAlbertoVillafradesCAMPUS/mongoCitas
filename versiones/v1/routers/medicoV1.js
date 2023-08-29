46f8128d8e7f7d31903b61333232e894ddbb07ea
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-08-28 20:58:55 -0500

    feat(EndPoint): :sparkles: 5 EndPoint
    se realizo el 5 endpoint que consiste en obteer todos los paciente con tiene citas con un medico especifico
    
    by:Carlos Villafrades

--- versiones/v1/routers/medicoV1.js
+++ versiones/v1/routers/medicoV1.js
@@ -31,7 +31,53 @@ medicoV1.get("/", validatePermisos("get_medicos"), async (req,res)=>{
     res.send(data)
 
 } catch (error) {
-    res.send({status: 400, message: "Error al consultar la collection"})
+  res.send({status: 400, message: "Error al obtener lo datos"})
+}
+})
+
+//5. Encontrar todos los pacientes que tienen citas con un médico específico (por ejemplo, el médico con **med_nroMatriculaProsional 1**) ?nroMatricula
+medicoV1.get("/usuarios", validatePermisos("get_medicos"), async (req,res)=>{
+  try {
+
+    let numero_matricula = req.query.nroMatricula
+    numero_matricula = parseInt(numero_matricula)
+
+    let tabla = dataBase.collection("usuario")
+    let data = await tabla.aggregate([
+        {
+            $lookup: {
+                from: "cita",
+                localField: "usu_id",
+                foreignField: "cit_datosUsuario",
+                as: "info_citas"
+            }
+        },
+        {
+            $lookup: {
+                from: "medico",
+                localField: "info_citas.cit_medico",
+                foreignField: "med_nroMatriculaProfesional",
+                as: "info_medico"
+            }
+        },
+        {
+            $match: {
+                "info_medico.med_nroMatriculaProfesional": numero_matricula
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                "info_medico.role":0,
+                "info_medico.password":0
+            }
+        }
+    ]).toArray();
+
+    res.send(data)
+
+}  catch (error) {
+  res.send({status: 400, message: "Error al obtener lo datos"})
 }
 })
 
