9b121e8a2cd9136d246f389eb3ba4b7a0bc4f44c
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-08-28 20:42:18 -0500

    feat(EndPoint): :sparkles: 4 EndPoint
    se realizo el cuarto endpoint que consistia en obtener todas las citas activas de cierto usuarios especifico
    
    by:Carlos Villafrades

--- versiones/v1/routers/usuariosV1.js
+++ versiones/v1/routers/usuariosV1.js
@@ -9,7 +9,6 @@ let usuarioV1 = Router();
 // 1.Obtener todos los pacientes alfabéticamente
 usuarioV1.get("/", validatePermisos("get_usuario"), async (req,res)=>{
     try {
-
         let tabla = dataBase.collection("usuario")
         let data = await tabla.aggregate([
             {
@@ -27,6 +26,53 @@ usuarioV1.get("/", validatePermisos("get_usuario"), async (req,res)=>{
         res.send(data)
 
     } catch (error) {
+        res.send({status: 400, message: "Error al obtener lo datos"})
+    }
+    
+})
+
+//4. Encontrar la próxima cita para un usuario específico (por ejemplo, el paciente con **usu_id 1**) ?id
+usuarioV1.get("/unico", validatePermisos("get_usuario"), async (req,res)=>{
+    try {  
+        let id = req.query.id
+        id = parseInt(id)
+
+        let tabla = dataBase.collection("usuario")
+        let data = await tabla.aggregate([
+            {
+                $match: {
+                    usu_id: id
+                }
+            },
+            {
+                $lookup: {
+                    from: "cita",
+                    localField: "usu_id",
+                    foreignField: "cit_datosUsuario",
+                    as: "info_Usuario"
+                }
+            },
+            {
+                $unwind: "$info_Usuario"
+            },
+            {
+                $match: {
+                    "info_Usuario.cit_estadoCita": "Activa"
+                }
+            },
+            { 
+                $project: {
+                    _id: 0,
+                    usu_acudiente: 0,
+                    "info_Usuario._id": 0,
+                    "info_Usuario.cit_datosUsuario": 0
+                }
+            }
+        ]).toArray();
+
+        res.send(data)
+
+    }  catch (error) {
         res.send({status: 400, message: "Error al obtener lo datos"})
     }
     
