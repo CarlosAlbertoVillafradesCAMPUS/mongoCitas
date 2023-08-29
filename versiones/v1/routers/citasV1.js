4646efcd7a2bed32f0e09d5d61a9ccee13384ffa
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-08-28 21:05:50 -0500

    feat(EndPoint): :sparkles: 6 EndPoint
    se realizo el 6 endpoint que consistia en obtener las consultorias de una paciente especifico
    
    by: Carlos Villafrades

--- versiones/v1/routers/citasV1.js
+++ versiones/v1/routers/citasV1.js
@@ -27,6 +27,47 @@ citasV1.get("/", validatePermisos("get_citas"), async (req,res)=>{
     
 })
 
+//6.Obtener las consultorías para un paciente específico (por ejemplo, paciente **con usu_id 1**) ?id=""
+citasV1.get("/usuario", validatePermisos("get_citas"), async (req,res)=>{
+    try {
+
+        let id = req.query.id
+        id = parseInt(id)
+
+        let tabla = dataBase.collection("usuario")
+        let data = await tabla.aggregate([
+            {
+                $match: {
+                    usu_id: id
+                }
+            },
+            {
+                $lookup: {
+                    from: "citas",
+                    localField: "usu_id",
+                    foreignField: "cit_datosUsuario",
+                    as: "info_citas"
+                }
+            },
+            { 
+                $project: {
+                    _id: 0,
+                    usu_acudiente: 0,
+                    "info_citas._id": 0,
+                    "info_citas.cit_datosUsuario": 0
+                }
+            }
+        ]).toArray();
+
+        res.send(data)
+
+    }  catch (error) {
+        res.send({status: 400, message: "Error al obtener lo datos"})
+    }
+    
+})
+
+
 export {
     citasV1
 }
